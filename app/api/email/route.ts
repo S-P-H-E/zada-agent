9ffc28f7c8e4da395f735ecf6767637f71771e98
S-P-H-E <sphe.g.personal@gmail.com>
2025-06-09 18:35:11 +0200

    first

---
+++ app/api/email/route.ts
@@ -0,0 +1,23 @@
+import { EmailTemplate } from '@/components/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'sphe@eightytwo.studio',
+      to: ['jairampranavwork@gmail.com'],
+      subject: 'Show me the snowbunnies please',
+      react: EmailTemplate({ firstName: 'Pranav' }) as React.ReactNode,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
