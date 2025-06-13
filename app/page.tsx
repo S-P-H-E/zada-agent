23ad235b2eb7227530b7144b2e68e2935a490d79
S-P-H-E <sphe.g.personal@gmail.com>
2025-06-13 18:53:38 +0200

    link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
 
   return (
-    <div>
-      home
+    <div className="h-[100dvh] flex flex-col items-center justify-center text-5xl gap-5">
+      Big man ting, this is the home page
+      <Link href="/565656" className="underline text-blue-500">
+      Try click this for a sample bot
+      </Link>
     </div>
   )
 }
